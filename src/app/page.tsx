13ccd7647dbf0f7a2319476d40d9125c520a6fd4
jhukal9@gmail.com <jhukal9@gmail.com>
2024-05-13 21:49:16 +0545

    updated two new section for blog post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,6 +134,116 @@ export default function Home() {
             </div>
           </div>
         </section>
+        <div className="section article-list mb-20">
+          <div className="container">
+            <div className="grid grid-cols-2 gap-x-8 gap-y-12">
+              <div className="col-span-2 lg:col-span-1">
+                <div className="article-list-item rectangle-on-hover">
+                  <Link href={"#"}>
+                    <div className="featured-image">
+                      <Image src={"/placeholder-08.jpg"} alt="" width={960} height={720} />
+                      <div className="date">
+                        <span>Dec 2, 2015</span>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="article-list-item-content">
+                    <div className="title text-center">
+                      <Link href={"#"} className="category">
+                        Life Style
+                      </Link>
+                      <Link href={"#"}>
+                        <h2>My Favorite New Travel Destinations</h2>
+                      </Link>
+                    </div>
+                    <p>
+                      Exercitation photo booth stumptown tote bag Banksy, Proin elit small batch freegan sed. Craft beer elit seitan exercitation,
+                      photo booth et 86-bit kale chips proident chillwave deep v laborum. Aliquip veniam delectus, Marfa eiusmod Pinterest…
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className="article-list-item rectangle-on-hover">
+                  <Link href={"#"} className="featured-image">
+                    <div className="featured-image">
+                      <Image src={"/placeholder-07.jpg"} alt="" width={960} height={720} />
+                      <div className="date">
+                        <span>Dec 2, 2015</span>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="article-list-item-content">
+                    <div className="title text-center">
+                      <Link href={"#"} className="category">
+                        Life Style
+                      </Link>
+                      <Link href={"#"}>
+                        <h2>Best hat that I've ever saw</h2>
+                      </Link>
+                    </div>
+                    <p>
+                      Exercitation photo booth stumptown tote bag Banksy, Proin elit small batch freegan sed. Craft beer elit seitan exercitation,
+                      photo booth et 86-bit kale chips proident chillwave deep v laborum. Aliquip veniam delectus, Marfa eiusmod Pinterest…
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className="article-list-item rectangle-on-hover">
+                  <Link href={"#"}>
+                    <div className="featured-image">
+                      <Image src={"/placeholder-08.jpg"} alt="" width={960} height={720} />
+                      <div className="date">
+                        <span>Dec 2, 2015</span>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="article-list-item-content">
+                    <div className="title text-center">
+                      <Link href={"#"} className="category">
+                        Life Style
+                      </Link>
+                      <Link href={"#"}>
+                        <h2>My Favorite New Travel Destinations</h2>
+                      </Link>
+                    </div>
+                    <p>
+                      Exercitation photo booth stumptown tote bag Banksy, Proin elit small batch freegan sed. Craft beer elit seitan exercitation,
+                      photo booth et 86-bit kale chips proident chillwave deep v laborum. Aliquip veniam delectus, Marfa eiusmod Pinterest…
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className="article-list-item rectangle-on-hover">
+                  <Link href={"#"} className="featured-image">
+                    <div className="featured-image">
+                      <Image src={"/placeholder-07.jpg"} alt="" width={960} height={720} />
+                      <div className="date">
+                        <span>Dec 2, 2015</span>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="article-list-item-content">
+                    <div className="title text-center">
+                      <Link href={"#"} className="category">
+                        Life Style
+                      </Link>
+                      <Link href={"#"}>
+                        <h2>Best hat that I've ever saw</h2>
+                      </Link>
+                    </div>
+                    <p>
+                      Exercitation photo booth stumptown tote bag Banksy, Proin elit small batch freegan sed. Craft beer elit seitan exercitation,
+                      photo booth et 86-bit kale chips proident chillwave deep v laborum. Aliquip veniam delectus, Marfa eiusmod Pinterest…
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <section className="latest mb-20 relative">
           <div className="container">
             <div className="section-title  absolute left-[50%] top-[-10px] px-4 bg-white translate-x-[-50%] ">
@@ -201,7 +311,123 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className="highlighted relative mb-20">
+          <div className="container">
+            <div className="section-title  absolute left-[50%] top-[-10px] px-4 bg-white translate-x-[-50%] ">
+              <h3 className=" text-[12px] md:text-[18px] tracking-[3px]">Latest Article</h3>
+            </div>
+            <div className=" highlighted-inner-box py-16 px-8 border-[1px] border-primary border-solid">
+              <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-x-4 gap-y-8 lg:gap-0">
+                <div className="col-span-1 ">
+                  <div className="text-on-hover">
+                    <Link href={"#"}>
+                      <div className="featured-image">
+                        <Image src={"/placeholder-09.jpg"} alt="" width={672} height={750} />
+                      </div>
+                    </Link>
+                    <div className="content">
+                      <Link href={"#"}>
+                        <h5>We got Married </h5>
+                      </Link>
+                      <Link href={"#"} className="category">
+                        Lifestyle
+                      </Link>
+                      <p className="author">
+                        By - <Link href={"#"}>Arjun Jhukal</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 ">
+                  <div className="text-on-hover">
+                    <Link href={"#"}>
+                      <div className="featured-image">
+                        <Image src={"/placeholder-10.jpg"} alt="" width={672} height={750} />
+                      </div>
+                    </Link>
+                    <div className="content">
+                      <Link href={"#"}>
+                        <h5>We got Married </h5>
+                      </Link>
+                      <Link href={"#"} className="category">
+                        Lifestyle
+                      </Link>
+                      <p className="author">
+                        By - <Link href={"#"}>Arjun Jhukal</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 ">
+                  <div className="text-on-hover">
+                    <Link href={"#"}>
+                      <div className="featured-image">
+                        <Image src={"/placeholder-11.jpg"} alt="" width={672} height={750} />
+                      </div>
+                    </Link>
+                    <div className="content">
+                      <Link href={"#"}>
+                        <h5>We got Married </h5>
+                      </Link>
+                      <Link href={"#"} className="category">
+                        Lifestyle
+                      </Link>
+                      <p className="author">
+                        By - <Link href={"#"}>Arjun Jhukal</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 ">
+                  <div className="text-on-hover">
+                    <Link href={"#"}>
+                      <div className="featured-image">
+                        <Image src={"/placeholder-04.jpg"} alt="" width={672} height={750} />
+                      </div>
+                    </Link>
+                    <div className="content">
+                      <Link href={"#"}>
+                        <h5>We got Married </h5>
+                      </Link>
+                      <Link href={"#"} className="category">
+                        Lifestyle
+                      </Link>
+                      <p className="author">
+                        By - <Link href={"#"}>Arjun Jhukal</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
+      <footer className="footer py-8">
+        <div className="container">
+          <div className="copy-right flex justify-between items-center">
+            <p className="text-[22px]">&copy; 2024 Arjun Jhukal. All Right Reserved.</p>
+            <ul className="flex justify-end items-center gap-8">
+              <li>
+                <Link href={"#"} className="text-[22px]">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"} className="text-[22px]">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"} className="text-[22px]">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
